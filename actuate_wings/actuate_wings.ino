e3e848a43126d045555b1e26d95ec989af4f47c5
margaretmcrawf <margaretmcrawf@gmail.com>
2017-10-20 10:31:30 -0400

    added flapping and average sensorval

--- actuate_wings/actuate_wings.ino
+++ actuate_wings/actuate_wings.ino
@@ -5,6 +5,7 @@ Servo servo;
 // pin values for sensors
 int analogPin0 = A0; // infrared sensor
 int currentpos;
+bool isOpening = true;
 
 void setup() {
   // start the serial
@@ -24,9 +25,53 @@ int sensorval_to_dist(int sensorVal) {
 }
 
 /*
- * Take distance, move the wing accordingly
+ * Get the average of given number of sensor readings, to account for 
+ * occasional inconsistencies in readings
  */
-void move_motor(int dist) {
+float average_distance_val(int reps) {
+  float total = 0;
+  for (int i = 0; i < reps; i++) {
+    // read the analog in value:
+    int sensorValue = analogRead(analogPin0);
+    // map it to a distance
+    float distance = sensorval_to_dist(sensorValue);
+    //add it to the total for averaging purposes
+    total += distance;
+    // wait 50 milliseconds before the next reading, to allow the
+    // sensor to regather the value
+    delay(50);
+  }
+  return total/reps;
+}
+
+/*
+ * Flap the wing (one of the states we might want)
+ */
+void flap(int dist) {
+  if (isOpening and dist < 300) {
+    if (currentpos < 89) {
+      currentpos += 5;
+      servo.write(currentpos);
+      delay(50);
+    } else {
+      isOpening = false;
+    }
+  else {
+    isOpening = false;
+    if (currentpos > 0) {
+      currentpos -= 5;
+      servo.write(currentpos);
+      delay(50);
+    } else {
+      isOpening = true;
+    }
+  }
+}
+
+/*
+ * Try to hold open position, close if something is close
+ */
+void stay_open(int dist) {
   if (dist > 100) {
     Serial.println("Too close! Retreat!");
     if (currentpos < 89) {
@@ -34,7 +79,8 @@ void move_motor(int dist) {
         servo.write(currentpos);
         delay(50);
     }
-  } else {
+  } 
+  else {
     Serial.println("I believe I can fly!");
     if (currentpos > 0) {
         currentpos -= 5;
@@ -44,11 +90,16 @@ void move_motor(int dist) {
   }
 }
 
+/*
+ * Take distance, move the wing accordingly
+ */
+void move_motor(int dist) {
+  flap(dist);
+}
+
 void loop() {
   // read from the sensor
-  int sensorValue = analogRead(analogPin0);
-  // convert the value to a distance
-  int distance = sensorval_to_dist(sensorValue);
+  int distance = average_distance_val(10);
   // move (bitch, get out the way)
   move_motor(distance);
 }
